b2ea0f1894d5f6c566270b469ec0c6199ae89100
bronson113 <40067845+bronson113@users.noreply.github.com>
2018-09-15 14:02:14 +0800

    Add files via upload

--- client.php
+++ client.php
@@ -1,5 +1,16 @@
 <?php
-  echo "hi";
+  if (!$_GET[order_date]){
+    printf("please choose the a date");
+    exit();
+  }
+  else if ($_GET[seatNumber]==0){
+    printf("please enter your seat number");
+    exit();
+  }
+  else if ($_GET[orders]==0){
+    printf("please choose what to eat");
+    exit();
+  }
   $mysqli = new mysqli("den1.mysql6.gear.host", "orderhistory", "Mk6925?Wx?43", "orderhistory");
   if ($mysqli->connect_errno){
     printf("no connection: %s\n", $mysqli->connect_error);
@@ -9,7 +20,8 @@
   $sql = "INSERT INTO everyonesorder (order_date, seatNumber, orders) VALUES ('$_GET[order_date]','$_GET[seatNumber]','$_GET[orders]')";
   if (!$mysqli->query($sql)){
     printf("error: %s\n", $mysqli->error);
+    exit();
   }
-  else echo "added";
+  else printf("Your order has been sended");
   $mysqli->close();
 ?>
