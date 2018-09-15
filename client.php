87cc35f7daf82ffe56b0e4faa06bc7bbca0afdeb
bronson113 <bronson1013@gmail.com>
2018-09-15 13:10:24 +0800

    git commit

--- client.php
+++ client.php
@@ -1,11 +1,11 @@
 <?php
   echo "hi";
-  $mysqli = new mysqli("den1.mysql6.gear.host", "orderhistory", "Mk6925?Wx?43", "lunch_orders");
+  $mysqli = new mysqli("den1.mysql6.gear.host", "orderhistory", "Mk6925?Wx?43", "orderhistory");
   if ($mysqli->connect_errno){
     printf("no connection: %s\n", $mysqli->connect_error);
     exit();
   }
-  $mysqli->select_db("everyonesorder");
+  $mysqli->select_db("orderhistory");
   $sql = "INSERT INTO everyonesorder (order_date, seatNumber, orders) VALUES ('$_GET[order_date]','$_GET[seatNumber]','$_GET[orders]')";
   if (!$mysqli->query($sql)){
     echo "no 2";
@@ -13,21 +13,4 @@
   }
   else echo "added";
   $mysqli->close();
-?>
-
-<?php
-$mysqli = new mysqli("localhost", "my_user", "my_password", "world");
-
-/* check connection */
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
-
-if (!$mysqli->query("SET a=1")) {
-    printf("Errormessage: %s\n", $mysqli->error);
-}
-
-/* close connection */
-$mysqli->close();
 ?>
